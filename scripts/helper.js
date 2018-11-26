62bb62b818ca6d6f9cbd00b55e6398e105173621
Meytal <meytalst@gmail.com>
2018-11-26 14:39:45 -0600

    Assignment 11: fourth try

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,6 +3,8 @@ class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
     const totalTime = player.getDuration();
+    const newSong = album.songs[previousSongIndex];
+    $('.total-time').text(player.newSong.duration);
     $('#time-control').text(player.totalTime);
   }
 }
